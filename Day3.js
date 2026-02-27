30f2b10eeb9aeb11e63c541428fa445118e210d1
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-02-26 23:45:39 -0500

    Day 3 ClassWork Done

--- Day3.js
+++ Day3.js
@@ -125,3 +125,47 @@
 // }
 
 // console.log(invertedPyramid(5));
+
+// function invertedPyramid(rows) {
+//   for (let i = rows * 2 - 1; i >= 1; i -= 2) {
+//     let star = "";
+//     let currentRow = (i + 1) / 2;
+
+//     for (let j = 1; j <= rows + currentRow - 1; j++) {
+//       if (j <= rows - currentRow) {
+//         star += " ";
+//       } else {
+//         star += "*";
+//       }
+//     }
+//     console.log(star);
+//   }
+
+//   return "Inverted Pyramid";
+// }
+
+// console.log(invertedPyramid(5));
+
+// **************************************************************************************************************************************
+
+// 5. Print Hollow Square Pattern
+
+// function hollowSquare(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     let star = "";
+
+//     for (let j = 1; j <= rows; j++) {
+//       if (i === 1 || i === rows || j === 1 || j === rows) {
+//         star += "*";
+//       } else {
+//         star += " ";
+//       }
+//     }
+
+//     console.log(star);
+//   }
+
+//   return "Hollow Square";
+// }
+
+// console.log(hollowSquare(5));

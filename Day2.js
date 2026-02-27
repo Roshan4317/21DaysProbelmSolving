56ee77df9284ff9f387ad2d74e11b51ab83ff009
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-02-26 23:01:35 -0500

    Day 2 Updated and Day 3 started

--- Day2.js
+++ Day2.js
@@ -52,66 +52,140 @@
 
 // 4. Check if a Character is a Vowel or Consonant
 
-// function checkChar(char) {
+// function checkChar(char = "0") {
 //   const vowels = "aeiou";
 
-//   if (
-//     !char.charCodeAt() >= 65 &&
-//     char.charCodeAt() <= 91 &&
-//     char.charCodeAt() >= 97 &&
-//     char.charCodeAt() <= 122
-//   ) {
-//     return "Enter a valid alphabet";
-//   }
-
-//   if (vowels.includes(char.toLowerCase())) {
+//   if (vowels.includes(char.toLowerCase()) && char !== "") {
 //     return "Vowel";
-//   } else {
+//   } else if (/^[a-z]$/.test(char.toLowerCase())) {
 //     return "Consonant";
+//   } else {
+//     return "Enter a valid Alphabet";
 //   }
 // }
 
 // console.log(checkChar(""));
 
+// function checkChar(char = "0") {
+//   if (
+//     char.toLowerCase().charCodeAt() === 97 ||
+//     char.toLowerCase().charCodeAt() === 101 ||
+//     char.toLowerCase().charCodeAt() === 105 ||
+//     char.toLowerCase().charCodeAt() === 111 ||
+//     char.toLowerCase().charCodeAt() === 117
+//   ) {
+//     return "Vowel";
+//   } else if (
+//     char.toLowerCase().charCodeAt() >= 97 &&
+//     char.toLowerCase().charCodeAt() <= 122
+//   ) {
+//     return "Consonant";
+//   }
+//   return "Enter a Valid Alphabet";
+// }
+
 //***************************************************************************************************************************
+
+// 5. Check if a Year is a Leap Year
+
+// function checkIfAYearIsLeapYear(year) {
+//   if (year % 400 === 0) {
+//     return "Leap Year";
+//   } else if (year % 100 === 0) {
+//     return "Not a Leap Year";
+//   } else if (year % 4 === 0) {
+//     return "Leap Year";
+//   }
+
+//   return "Not Leap Year";
+// }
+
+// console.log(checkIfAYearIsLeapYear(1900));
+
+// function checkIfAYearIsLeapYear(year) {
+//   if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+//     return "Leap Year";
+//   }
+
+//   return "Not Leap Year";
+// }
+
+//***************************************************************************************************************************
+
+// HomeWork
 
 // 1. Check if a Character is Uppercase, Lowercase, Digit, or Special Character
 
-// function checkChar(ch) {
-//   if (ch >= "A" && ch <= "Z") {
-//     return "Uppercase Letter";
-//   } else if (ch >= "a" && ch <= "z") {
-//     return "Lowercase Letter";
-//   } else if (ch >= "0" && ch <= "9") {
+// function checkChar(char) {
+//   const charCode = char.charCodeAt();
+
+//   if (charCode >= 65 && charCode <= 90) {
+//     return "Upper Case";
+//   } else if (charCode >= 97 && charCode <= 122) {
+//     return "Lower Case";
+//   } else if (charCode >= 48 && charCode <= 57) {
 //     return "Digit";
-//   } else {
+//   } else if (
+//     (charCode >= 58 && charCode <= 64) ||
+//     (charCode >= 32 && charCode <= 47)
+//   ) {
 //     return "Special Character";
 //   }
+
+//   return "Enter a Valid Characters Only";
 // }
 
 // console.log(checkChar("A"));
 // console.log(checkChar("b"));
 // console.log(checkChar("5"));
 // console.log(checkChar("#"));
+// console.log(checkChar(""));
 
 //***************************************************************************************************************************
 
-// 2. Check Triangle Type Using Sides and Angles
+// 2. Check Triangle Type Using Sides
 
 // function triangleBySides(a, b, c) {
-//   if (a + b <= c || a + c <= b || b + c <= a) {
-//     return "Not a valid triangle";
-//   }
+//   const [side1, side2, hypo] = [a, b, c].sort((a, b) => a - b);
 
-//   if (a === b && b === c) {
+//   if (side1 ** 2 + side2 ** 2 === hypo ** 2) {
+//     return "Right-angled Triangle";
+//   } else if (a === b && b === c) {
 //     return "Equilateral Triangle";
 //   } else if (a === b || b === c || a === c) {
 //     return "Isosceles Triangle";
-//   } else {
-//     return "Scalene Triangle";
 //   }
+//   return "Scalene Triangle";
 // }
 
 // console.log(triangleBySides(5, 5, 5));
 // console.log(triangleBySides(5, 5, 3));
 // console.log(triangleBySides(3, 4, 5));
+// console.log(triangleBySides(9, 7, 4));
+
+//***************************************************************************************************************************
+
+// 3. Calculate Income Tax Based on Slabs
+
+// Up to $2,50,000 - No Tax
+// $2,50,001-$5,00,000 - 5%
+// $5,00,001 - $ 10,00,000 -20%
+// Above $10,00,000 -30%
+
+// function incomeTaxCalculator(income) {
+//   if (income <= 0) {
+//     return "Enter valid Amount";
+//   }
+
+//   if (income <= 250000) {
+//     return 0;
+//   } else if (income <= 500000) {
+//     return (income - 250000) * 0.05;
+//   } else if (income <= 1000000) {
+//     return 250000 * 0.05 + (income - 500000) * 0.2;
+//   } else {
+//     return 250000 * 0.05 + 500000 * 0.2 + (income - 1000000) * 0.3;
+//   }
+// }
+
+// console.log(incomeTaxCalculator(300000));
---
+++ Day3.js
@@ -0,0 +1,127 @@
+// ClassWork
+
+// 1. Print Right-Angled Star Triangle
+
+// function printRightAnglesTriangle(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     let star = "";
+
+//     for (let j = 1; j <= i; j++) {
+//       star += "*";
+//     }
+
+//     console.log(star);
+//   }
+
+//   return "Right-Angled Star Triangle";
+// }
+
+// console.log(printRightAnglesTriangle(8));
+
+// **************************************************************************************************************************************
+
+// 2. Print Inverted Right-Angled Triangle
+
+// function invertedRightAngledTriangle(rows) {
+//   for (let i = rows; i >= 1; i--) {
+//     let star = "";
+//     for (let j = 1; j <= i; j++) {
+//       star += "*";
+//     }
+
+//     console.log(star);
+//   }
+
+//   return "invertedRightAngledTriangle";
+// }
+
+// console.log(invertedRightAngledTriangle(5));
+
+// **************************************************************************************************************************************
+
+// 3. Print Pyramid Pattern
+
+// function pyramid(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     let star = "";
+
+//     for (let j = 1; j <= rows * 2 - 1; j++) {
+//       if (j <= rows - i || j > rows + i - 1) {
+//         star += "|";
+//       } else {
+//         star += "*";
+//       }
+//     }
+
+//     console.log(star);
+//   }
+
+//   return "Pyramid Pattern ";
+// }
+
+// console.log(pyramid(5));
+
+// function pyramid(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     let star = "";
+
+//     for (let j = 1; j <= rows + i - 1; j++) {
+//       if (j <= rows - i) {
+//         star += " ";
+//       } else {
+//         star += "*";
+//       }
+//     }
+
+//     console.log(star);
+//   }
+
+//   return "Pyramid Pattern ";
+// }
+
+// console.log(pyramid(5));
+
+// function pyramid(rows) {
+//   for (let i = 1; i <= rows * 2 - 1; i += 2) {
+//     let star = "";
+
+//     let currentRow = (i + 1) / 2;
+
+//     for (let j = 1; j <= rows + currentRow - 1; j++) {
+//       if (j <= rows - currentRow) {
+//         star += " ";
+//       } else {
+//         star += "*";
+//       }
+//     }
+
+//     console.log(star);
+//   }
+
+//   return "Pyramid Pattern ";
+// }
+
+// console.log(pyramid(5));
+
+// **************************************************************************************************************************************
+
+// 4. Print Inverted Pyramid Pattern
+
+// function invertedPyramid(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     let star = "";
+//     for (let j = 1; j <= rows * 2 - i; j++) {
+//       if (j < i) {
+//         star += " ";
+//       } else {
+//         star += "*";
+//       }
+//     }
+
+//     console.log(star);
+//   }
+
+//   return "Inverted Pyramid";
+// }
+
+// console.log(invertedPyramid(5));

5cb5359ef7befc629693e1e933bd77851854f4ad
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-02-27 23:13:10 -0500

    Print Hollow Inverted Pyramid

--- Day3.js
+++ Day3.js
@@ -174,10 +174,9 @@
 
 // 6. Print Hollow Pyramid Pattern
 
-// function hollowPyramid(rows) {
-//   for (let i = 1; i <= rows; i++) {
+// function hollowpyramid(rows) {
+//   for (let i = 1; i <= 5; i++) {
 //     let star = "";
-
 //     for (let j = 1; j <= rows * 2 - 1; j++) {
 //       if (j === rows - i + 1 || j === rows + i - 1 || i === rows) {
 //         star += "*";
@@ -188,8 +187,76 @@
 
 //     console.log(star);
 //   }
+//   return "Hollow Pyramid";
+// }
 
+// console.log(hollowpyramid(5));
+
+// function hollowPyramid(rows) {
+//   for (let i = 1; i <= rows * 2 - 1; i += 2) {
+//     let currentRow = (i + 1) / 2;
+
+//     let star = "";
+//     for (let j = 1; j <= rows + currentRow - 1; j++) {
+//       if (
+//         j <= rows - currentRow ||
+//         (j > rows - currentRow + 1 &&
+//           j < rows + currentRow - 1 &&
+//           i !== rows * 2 - 1)
+//       ) {
+//         star += " ";
+//       } else {
+//         star += "*";
+//       }
+//     }
+
+//     console.log(star);
+//   }
 //   return "Hollow Pyramid";
 // }
 
 // console.log(hollowPyramid(5));
+
+// **************************************************************************************************************************************
+
+// 7 . Print Alternating Binary Triangle
+
+// function AlternatingBinaryTriangle(rows) {
+//   for (let i = 1; i <= rows; i++) {
+//     let count = "";
+
+//     for (let j = 1; j <= i; j++) {
+//       count += i % 2 === 1 ? j % 2 : (j + 1) % 2;
+//     }
+
+//     console.log(count);
+//   }
+
+//   return "AlternatingBinaryTriangle";
+// }
+
+// console.log(AlternatingBinaryTriangle(5));
+
+// **************************************************************************************************************************************
+
+// HomeWork
+
+// 1. Print Hollow Inverted Pyramid
+
+function hollowInvertedPyramid(rows) {
+  for (let i = rows; i >= 1; i--) {
+    let line = " ".repeat(rows - i);
+
+    if (i === rows) {
+      line += "*".repeat(rows * 2 - 1);
+    } else if (i === 1) {
+      line += "*";
+    } else {
+      line += "*" + " ".repeat(2 * i - 3) + "*";
+    }
+
+    console.log(line);
+  }
+}
+
+hollowInvertedPyramid(5);

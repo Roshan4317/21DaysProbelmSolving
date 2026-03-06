fe64cb9dd59a51f24f152690d88f824254ccc804
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-05 23:32:35 -0500

    GreatestCommonFactor

--- Day5.js
+++ Day5.js
@@ -30,20 +30,92 @@
 
 // console.log(allFactors(24));
 
-function getAllFactors(num) {
-  const factors = [];
+// function getAllFactors(num) {
+//   const factors = [];
 
-  let loopCount = 0;
-  for (let i = 1; i <= Math.sqrt(num); i++) {
-    loopCount++;
-    if (num % i === 0) {
-      factors.splice(factors.length / 2, 0, i);
-      const otherPair = num / i;
-      if (i !== otherPair) factors.splice(factors.length / 2 + 1, 0, otherPair);
-    }
-  }
-  console.log({ num, loopCount });
-  return factors;
-}
+//   for (let i = 1; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       factors.splice(factors.length / 2, 0, i);
+//       const anotherFactor = num / i;
+//       if (i != anotherFactor)
+//         factors.splice(factors.length / 2 + 1, 0, anotherFactor);
+//     }
+//   }
 
-console.log(getAllFactors(100));
+//   return factors;
+// }
+
+// console.log(getAllFactors(100));
+
+// *********************************************************************************************
+
+// 2. Print All Multiples of a Number up to N
+
+// function multiplesOfNumber(num, limit) {
+//   let multiples = [];
+
+//   for (let i = num; i <= limit; i += num) {
+//     multiples.push(i);
+//   }
+
+//   return multiples;
+// }
+
+// console.log(multiplesOfNumber(4, 30));
+
+// function multiplesOfNumber(num, limit) {
+//   let multiples = [];
+
+//   for (let i = 1; i <= limit / num; i++) {
+//     multiples.push(num * i);
+//   }
+
+//   return multiples;
+// }
+
+// console.log(multiplesOfNumber(4, 30));
+
+// *********************************************************************************************
+
+// 3. Find the HCF (Highest Common Factor) or GCD of Two Numbers
+
+// function HCF(num1, num2) {
+//   const factorsOFNum1 = [];
+//   let commonFactor = 0;
+
+//   for (let i = 1; i <= Math.sqrt(num1); i++) {
+//     if (num1 % i === 0) {
+//       factorsOFNum1.splice(factorsOFNum1.length / 2, 0, i);
+//       const pairFactor = num1 / i;
+//       if (i != pairFactor)
+//         factorsOFNum1.splice(factorsOFNum1.length / 2 + 1, 0, pairFactor);
+//     }
+//   }
+
+//   for (let i = factorsOFNum1.length - 1; i >= 1; i--) {
+//     if (num2 % factorsOFNum1[i] === 0) {
+//       return (commonFactor = factorsOFNum1[i]);
+//     }
+//   }
+
+//   return commonFactor;
+// }
+
+// console.log(HCF(12, 16));
+// console.log(HCF(12, 18));
+
+// function HCF(num1, num2) {
+//   let dividend = Math.max(num1, num2);
+//   let divisor = Math.min(num1, num2);
+
+//   while (dividend % divisor != 0) {
+//     const remainder = dividend % divisor;
+//     dividend = divisor;
+//     divisor = remainder;
+//   }
+
+//   return divisor;
+// }
+
+// console.log(HCF(12, 16));
+// console.log(HCF(12, 18));

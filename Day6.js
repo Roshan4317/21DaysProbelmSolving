4879f1ce63f366a6d49f9f08c1b0ebc1ddbe4476
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-06 23:05:50 -0500

    Check if two Numbers are Co-Prim

--- Day6.js
+++ Day6.js
@@ -2,15 +2,39 @@
 
 // 1. Check if a number is Prime or not
 
-function isPrime(num) {
-  if (num <= 1) return false;
+// function isPrime(num) {
+//   if (num <= 1) return false;
 
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false;
-  }
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) return false;
+//   }
 
-  return true;
-}
+//   return true;
+// }
 
-console.log(isPrime(7));
-console.log(isPrime(10));
+// console.log(isPrime(7));
+// console.log(isPrime(10));
+
+// *********************************************************************************************
+
+// 2. Check if two Numbers are Co-Prime
+
+// function HCF(a, b) {
+//   let divisor = a < b ? a : b;
+//   let dividend = a > b ? a : b;
+
+//   while (dividend % divisor !== 0) {
+//     let remainder = dividend % divisor;
+//     dividend = divisor;
+//     divisor = remainder;
+//   }
+
+//   return divisor;
+// }
+
+// function areCoPrime(num1, num2) {
+//   return HCF(num1, num2) === 1;
+// }
+
+// console.log(areCoPrime(8, 15));
+// console.log(areCoPrime(8, 12));

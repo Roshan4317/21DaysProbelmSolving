ec8de3c5196985ec3140f8cd8a84ce3342f1e3af
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-10 23:45:17 -0400

    Prime Factorization In Exponent Form

--- Day7.js
+++ Day7.js
@@ -2,25 +2,56 @@
 
 // 1. Find the Prime Factorization (Return as an Array)
 
-function primeFactorization(num) {
-  const primeFactors = [];
+// function primeFactorization(num) {
+//   const primeFactors = [];
 
+//   while (num % 2 === 0) {
+//     primeFactors.push(2);
+//     num = num / 2;
+//   }
+
+//   let factor = 3;
+//   while (factor <= Math.sqrt(num)) {
+//     while (num % factor === 0) {
+//       primeFactors.push(factor);
+//       num = num / factor;
+//     }
+//     factor += 2;
+//   }
+
+//   if (num > 2) primeFactors.push(num);
+//   return primeFactors;
+// }
+
+// console.log(primeFactorization(120));
+
+// *********************************************************************************************
+
+// 2. Factorization in Exponent Form
+function primeFactorizationInExponentForm(num) {
+  const primeFactors = {};
   while (num % 2 === 0) {
-    primeFactors.push(2);
+    primeFactors[2] = (primeFactors[2] || 0) + 1;
     num = num / 2;
   }
 
   let factor = 3;
   while (factor <= Math.sqrt(num)) {
     while (num % factor === 0) {
-      primeFactors.push(factor);
+      primeFactors[factor] = (primeFactors[factor] || 0) + 1;
       num = num / factor;
     }
     factor += 2;
   }
 
-  if (num > 2) primeFactors.push(num);
-  return primeFactors;
+  if (num > 2) primeFactors[num] = (primeFactors[num] || 0) + 1;
+  let output = "";
+  Object.keys(primeFactors).forEach((key, i) => {
+    output +=
+      `${key}^${primeFactors[key]}` +
+      (i !== Object.keys(primeFactors).length - 1 ? " x " : "");
+  });
+  return output;
 }
 
-console.log(primeFactorization(120));
+console.log(primeFactorizationInExponentForm(160));

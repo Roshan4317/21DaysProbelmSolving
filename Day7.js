52da1765dd9abb2235bd4f8f7e5ee018dc93c844
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-11 23:53:05 -0400

    Product of Distinct Primes

--- Day7.js
+++ Day7.js
@@ -146,25 +146,50 @@
 
 // 4. Check if a Number Is a Powerful Number
 
-const primeFactors = {};
-function primeFactorizationInExponentForm(num) {
+// const primeFactors = {};
+// function primeFactorizationInExponentForm(num) {
+//   while (num % 2 === 0) {
+//     primeFactors[2] = (primeFactors[2] || 0) + 1;
+//     num = num / 2;
+//   }
+
+//   let factor = 3;
+//   while (factor <= Math.sqrt(num)) {
+//     while (num % factor === 0) {
+//       primeFactors[factor] = (primeFactors[factor] || 0) + 1;
+//       num = num / factor;
+//     }
+//     factor += 2;
+//   }
+
+//   if (num > 2) primeFactors[num] = (primeFactors[num] || 0) + 1;
+
+//   return Math.min(...Object.values(primeFactors)) >= 2;
+// }
+
+// console.log(primeFactorizationInExponentForm(35));
+
+// *********************************************************************************************
+
+// 6.  Find the Product of All Distinct Prime Factors
+
+function productOfDistinctPrimes(num) {
+  const primeFactors = [];
   while (num % 2 === 0) {
-    primeFactors[2] = (primeFactors[2] || 0) + 1;
+    primeFactors.push(2);
     num = num / 2;
   }
-
   let factor = 3;
   while (factor <= Math.sqrt(num)) {
     while (num % factor === 0) {
-      primeFactors[factor] = (primeFactors[factor] || 0) + 1;
+      primeFactors.push(factor);
       num = num / factor;
     }
     factor += 2;
   }
-
-  if (num > 2) primeFactors[num] = (primeFactors[num] || 0) + 1;
-
-  return Math.min(...Object.values(primeFactors)) >= 2;
+  if (num > 2) primeFactors.push(num);
+  const distinctPrimes = [...new Set(primeFactors)];
+  return distinctPrimes.reduce((acc, val) => acc * val, 1);
 }
 
-console.log(primeFactorizationInExponentForm(35));
+console.log(productOfDistinctPrimes(84));

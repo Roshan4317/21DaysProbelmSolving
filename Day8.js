c65fd4b16069a3b8b3561707ad4f34b236e5adfc
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-15 21:53:21 -0400

    Count Frequency of Each Character

--- Day8.js
+++ Day8.js
@@ -15,17 +15,33 @@
 
 // 2. Check if a String is a Palindrome
 
-function isPalindrome(str) {
-  let leftIndex = 0;
-  let rightIndex = str.length - 1;
+// function isPalindrome(str) {
+//   let leftIndex = 0;
+//   let rightIndex = str.length - 1;
 
-  while (leftIndex < rightIndex) {
-    if (str[leftIndex] !== str[rightIndex]) return false;
-    leftIndex++;
-    rightIndex--;
+//   while (leftIndex < rightIndex) {
+//     if (str[leftIndex] !== str[rightIndex]) return false;
+//     leftIndex++;
+//     rightIndex--;
+//   }
+
+//   return true;
+// }
+
+// console.log(isPalindrome("racecar"));
+
+// *********************************************************************************************
+
+// 3.  Count Frequency of Each Character
+
+function countFrequency(str) {
+  const frequency = {};
+
+  for (let i = 0; i < str.length; i++) {
+    frequency[str[i]] = (frequency[str[i]] || 0) + 1;
   }
 
-  return true;
+  return frequency;
 }
 
-console.log(isPalindrome("racecar"));
+console.log(countFrequency("banana"));

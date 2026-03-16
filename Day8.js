d7ae0c90c53dbeb97b8dd8defa1514b36e3e37c1
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-15 21:51:20 -0400

    Check if a String is a Palindrome

--- Day8.js
+++ Day8.js
@@ -1,12 +1,31 @@
 // 1. Reverse a String (Manual Method Only)
-function reverseString(str) {
-  let reversedString = "";
+// function reverseString(str) {
+//   let reversedString = "";
 
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversedString += str[i];
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedString += str[i];
+//   }
+
+//   return reversedString;
+// }
+
+// console.log(reverseString("abcd"));
+
+// *********************************************************************************************
+
+// 2. Check if a String is a Palindrome
+
+function isPalindrome(str) {
+  let leftIndex = 0;
+  let rightIndex = str.length - 1;
+
+  while (leftIndex < rightIndex) {
+    if (str[leftIndex] !== str[rightIndex]) return false;
+    leftIndex++;
+    rightIndex--;
   }
 
-  return reversedString;
+  return true;
 }
 
-console.log(reverseString("abcd"));
+console.log(isPalindrome("racecar"));

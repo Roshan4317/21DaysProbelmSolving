baa81516463c47098e832243464c75fab7e6c8c1
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-20 23:08:07 -0400

    Manual Substring Search

--- Day9.js
+++ Day9.js
@@ -21,23 +21,46 @@
 
 // console.log(isSubstringOfAnother("heltlotheere", "there"));
 
-function isSubstringOfAnother(mainString, testString) {
-  for (let i = 0; i < mainString.length; i++) {
-    let char = mainString[i];
+// function isSubstringOfAnother(mainString, testString) {
+//   for (let i = 0; i < mainString.length; i++) {
+//     let char = mainString[i];
 
-    if (char === testString[0]) {
-      let match = true;
-      for (let j = 1; j < testString.length; j++) {
-        if (mainString[i + j] !== testString[j]) {
-          match = false;
-          break;
-        }
+//     if (char === testString[0]) {
+//       let match = true;
+//       for (let j = 1; j < testString.length; j++) {
+//         if (mainString[i + j] !== testString[j]) {
+//           match = false;
+//           break;
+//         }
+//       }
+//       if (match) return true;
+//     }
+//   }
+
+//   return false;
+// }
+
+// console.log(isSubstringOfAnother("heltlotheere", "there"));
+
+// *********************************************************************************************
+
+// 2.  Manual Substring Search (Without Using indexOf)
+
+function findSubstring(text, pattern) {
+  for (let i = 0; i <= text.length - pattern.length; i++) {
+    let match = true;
+
+    for (let j = 0; j < pattern.length; j++) {
+      if (text[i + j] !== pattern[j]) {
+        match = false;
+        break;
       }
-      if (match) return true;
     }
+
+    if (match) return i;
   }
 
-  return false;
+  return -1;
 }
 
-console.log(isSubstringOfAnother("heltlotheere", "there"));
+console.log(findSubstring("hello world", "wor"));

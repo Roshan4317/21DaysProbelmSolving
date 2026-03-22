3ee0f39662655a4cffe93be5d4e644934a43c7f6
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-21 23:41:29 -0400

    Updated on Day 9

--- Day9.js
+++ Day9.js
@@ -42,6 +42,23 @@
 
 // console.log(isSubstringOfAnother("heltlotheere", "there"));
 
+// function isSubstringOfAnother(mainString, testString) {
+//   for (let i = 0; i <= mainString.length - testString.length; i++) {
+//     let match = true;
+
+//     for (let j = 0; j < testString.length; j++) {
+//       if (mainString[i + j] !== testString[j]) {
+//         match = false;
+//         break;
+//       }
+//     }
+//     if (match) return true;
+//   }
+
+//   return false;
+// }
+// console.log(isSubstringOfAnother("heltlothere", "there"));
+
 // *********************************************************************************************
 
 // 2.  Manual Substring Search (Without Using indexOf)
@@ -69,27 +86,27 @@
 
 // 3 . Check if One String is Rotation of Another
 
-function isSubstring(text, pattern) {
-  for (let i = 0; i <= text.length - pattern.length; i++) {
-    let match = true;
+// function isSubstring(text, pattern) {
+//   for (let i = 0; i <= text.length - pattern.length; i++) {
+//     let match = true;
 
-    for (let j = 0; j < pattern.length; j++) {
-      if (text[i + j] !== pattern[j]) {
-        match = false;
-        break;
-      }
-    }
+//     for (let j = 0; j < pattern.length; j++) {
+//       if (text[i + j] !== pattern[j]) {
+//         match = false;
+//         break;
+//       }
+//     }
 
-    if (match) return true;
-  }
-  return false;
-}
+//     if (match) return true;
+//   }
+//   return false;
+// }
 
-function isRotation(s1, s2) {
-  if (s1.length !== s2.length) return false;
+// function isRotation(s1, s2) {
+//   if (s1.length !== s2.length) return false;
 
-  const combined = s1 + s1;
-  return isSubstring(combined, s2);
-}
+//   const combined = s1 + s1;
+//   return isSubstring(combined, s2);
+// }
 
-console.log(isRotation("abcde", "cdeab"));
+// console.log(isRotation("abcde", "cdeab"));

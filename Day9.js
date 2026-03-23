8c18504bde9611c81c1a8a0f5dc3f1a9ed7e7fb0
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-22 23:46:41 -0400

    isRotation

--- Day9.js
+++ Day9.js
@@ -81,8 +81,28 @@ function SubstringSearch(mainString, testString) {
   return "Did not find the testString";
 }
 
-console.log(SubstringSearch("hellotheredde", "there"));
+console.log(SubstringSearch("hellotheredde", "ac"));
 
 // *********************************************************************************************
 
 // 3 . Check if One String is Rotation of Another
+function isRotation(a, b) {
+  if (a.length !== b.length) return false;
+  a = a + a;
+
+  for (let i = 0; i <= a.length - b.length; i++) {
+    let match = true;
+
+    for (let j = 0; j < b.length; j++) {
+      if (a[i + j] !== b[j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) return true;
+  }
+
+  return false;
+}
+
+console.log(isRotation("abcde", "cdeab"));

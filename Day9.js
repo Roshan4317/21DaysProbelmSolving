682d51f012b1517163c8fa68423be26d20642d64
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-23 23:14:58 -0400

    Count Frequency of Each Word in a Sentence

--- Day9.js
+++ Day9.js
@@ -63,46 +63,52 @@
 
 // 2.  Manual Substring Search (Without Using indexOf)
 
-function SubstringSearch(mainString, testString) {
-  for (let i = 0; i <= mainString.length - testString.length; i++) {
-    let count = i;
-    let match = true;
-    for (let j = 0; j < testString.length; j++) {
-      if (mainString[i + j] !== testString[j]) {
-        match = false;
-        break;
-      }
-    }
-    if (match) {
-      count = i;
-      return `Found at index ${count}`;
-    }
-  }
-  return "Did not find the testString";
-}
+// function SubstringSearch(mainString, testString) {
+//   for (let i = 0; i <= mainString.length - testString.length; i++) {
+//     let count = i;
+//     let match = true;
+//     for (let j = 0; j < testString.length; j++) {
+//       if (mainString[i + j] !== testString[j]) {
+//         match = false;
+//         break;
+//       }
+//     }
+//     if (match) {
+//       count = i;
+//       return `Found at index ${count}`;
+//     }
+//   }
+//   return "Did not find the testString";
+// }
 
-console.log(SubstringSearch("hellotheredde", "ac"));
+// console.log(SubstringSearch("hellotheredde", "ac"));
 
 // *********************************************************************************************
 
 // 3 . Check if One String is Rotation of Another
-function isRotation(a, b) {
-  if (a.length !== b.length) return false;
-  a = a + a;
+// function isRotation(a, b) {
+//   if (a.length !== b.length) return false;
+//   a = a + a;
 
-  for (let i = 0; i <= a.length - b.length; i++) {
-    let match = true;
+//   for (let i = 0; i <= a.length - b.length; i++) {
+//     let match = true;
 
-    for (let j = 0; j < b.length; j++) {
-      if (a[i + j] !== b[j]) {
-        match = false;
-        break;
-      }
-    }
-    if (match) return true;
-  }
+//     for (let j = 0; j < b.length; j++) {
+//       if (a[i + j] !== b[j]) {
+//         match = false;
+//         break;
+//       }
+//     }
+//     if (match) return true;
+//   }
 
-  return false;
-}
+//   return false;
+// }
 
-console.log(isRotation("abcde", "deab"));
+// console.log(isRotation("abcde", "deab"));
+
+// **********************************************************************************************************
+
+// HomeWork
+
+//Count Frequency of Each Word in a Sentence

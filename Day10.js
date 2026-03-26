01ea95bc090b5ba15ffbdf9248e62eb7bf07f1d6
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-25 21:10:44 -0400

    Check if an Array is Sorted (Ascending)

--- Day10.js
+++ Day10.js
@@ -24,21 +24,53 @@
 
 // 2.  Reverse an Array (Manual Method Only)
 
-function reverseAnArray(arr) {
-  let left = 0;
-  let right = arr.length - 1;
-  (1, 3);
+// function reverseAnArray(arr) {
+//   let left = 0;
+//   let right = arr.length - 1;
+//   (1, 3);
 
-  while (left < right) {
-    let arrayLeft = arr[left];
-    arr[left] = arr[right];
-    arr[right] = arrayLeft;
+//   while (left < right) {
+//     let arrayLeft = arr[left];
+//     arr[left] = arr[right];
+//     arr[right] = arrayLeft;
 
-    left++;
-    right--;
+//     left++;
+//     right--;
+//   }
+
+//   return arr;
+// }
+
+// console.log(reverseAnArray([1, 2, 3, 4, 5]));
+
+// **********************************************************************************************************
+
+// 3. Check if an Array is Sorted (Ascending)
+
+// function checkIfSorted(arr) {
+//   const originalArray = [...arr];
+
+//   const result = arr.sort((a, b) => a - b);
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (originalArray[i] !== result[i]) {
+//       return "Not Sorted";
+//     }
+//   }
+
+//   return "Sorted";
+// }
+
+// console.log(checkIfSorted([1, 2, 3, 4, 5]));
+
+function checkIfSorted(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
   }
 
-  return arr;
+  return true;
 }
 
-console.log(reverseAnArray([1, 2, 3, 4, 5]));
+console.log(checkIfSorted([1, 2, 3, 4, 5]));

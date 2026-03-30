71d6c5d4c75e72208c3154d6d259bee3590ce82a
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-03-29 22:32:21 -0400

    Move All Zeroes to End (Stable, O(n))

--- Day11.js
+++ Day11.js
@@ -36,3 +36,44 @@
 // console.log(findMissingNumber([1, 3, 4, 2, 6]));
 
 // **********************************************************************************************************
+
+// 2. Move All Zeroes to End (Stable, O(n))
+
+// function moveAllZeros(arr) {
+//   let newArray = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//       newArray.push(arr[i]);
+//     }
+//   }
+
+//   let numberOfZeros = arr.length - newArray.length;
+
+//   for (let i = 0; i < numberOfZeros; i++) {
+//     newArray.push(0);
+//   }
+
+//   return newArray;
+// }
+
+// console.log(moveAllZeros([0, 1, 0, 3, 12, 0, 3, 5]));
+
+function moveAllZeros(arr) {
+  let position = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      arr[position] = arr[i];
+      position++;
+    }
+  }
+
+  for (let i = position; i < arr.length; i++) {
+    arr[i] = 0;
+  }
+
+  return arr;
+}
+
+console.log(moveAllZeros([0, 1, 0, 3, 12, 0, 5]));

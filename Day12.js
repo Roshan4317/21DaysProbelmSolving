d33fe09df5b432340ec32ff4689dde93958751c1
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-04-07 22:44:30 -0400

    Sort an Array in Ascending Order Using Selection Sort

--- Day12.js
+++ Day12.js
@@ -43,6 +43,25 @@
 
 // console.log(ascendingArray([5, 4, 13, 2, 1]));
 
+// function ascendingArray(arr) {
+//   for (let j = 0; j < arr.length - 1; j++) {
+//     let sorted = true;
+
+//     for (let i = 1; i < arr.length - j; i++) {
+//       if (arr[i] < arr[i - 1]) {
+//         sorted = false;
+//         let temp = arr[i];
+//         arr[i] = arr[i - 1];
+//         arr[i - 1] = temp;
+//       }
+//     }
+//     if (sorted) break;
+//   }
+
+//   return arr;
+// }
+
+// console.log(ascendingArray([5, 3, 8, 4, 2]));
 // ***********************************************************************************************************
 
 // 2. Sort an Array in Descending Order Using Bubble Sort
@@ -65,6 +84,25 @@
 // }
 
 // console.log(descendingArray([7, 1, 5, 3, 2, 12]));
+
+// function descendingArray(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let swap = false;
+//     for (let j = 0; j < arr.length - 1; j++) {
+//       if (arr[j + 1] > arr[j]) {
+//         swap = true;
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//     if (!swap) break;
+//   }
+
+//   return arr;
+// }
+
+// console.log(descendingArray([7, 12, 5, 3, 2, 100, 43, 15, 13]));
 
 // ***********************************************************************************************************
 
@@ -94,6 +132,24 @@
 
 // console.log(ascendingArray("javascript"));
 
+// function shortStringAlphabetically(str) {
+//   const splitted = str.split("");
+
+//   for (let i = 0; i < splitted.length - 1; i++) {
+//     for (let j = 0; j < splitted.length - 1 - i; j++) {
+//       if (splitted[j] > splitted[j + 1]) {
+//         let temp = splitted[j];
+//         splitted[j] = splitted[j + 1];
+//         splitted[j + 1] = temp;
+//       }
+//     }
+//   }
+
+//   return splitted.join("");
+// }
+
+// console.log(shortStringAlphabetically("javascript"));
+
 // ***********************************************************************************************************
 
 // 4. Sort an Array and Count the Number of Swaps Performed
@@ -113,31 +169,97 @@
 // console.log("Sorted:", arr);
 // console.log("Swaps:", swaps);
 
+// function sortAndCountComparisons(arr) {
+//   let count = 0;
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let swap = false;
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap = true;
+//         count++;
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//     if (!swap) break;
+//   }
+
+//   return { arr, count };
+// }
+
+// console.log(sortAndCountComparisons([4, 3, 2, 1]));
+
 // ***********************************************************************************************************
 
 // 5. Sort an Array of Objects by Age (Ascending)
 
-// let people = [
-//   { name: "A", age: 25 },
-//   { name: "B", age: 20 },
-//   { name: "C", age: 30 },
-// ];
+// function shortByAge(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let swap = false;
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       if (arr[j].age > arr[j + 1].age) {
+//         swap = true;
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
 
-// people.sort((a, b) => a.age - b.age);
+//     if (!swap) break;
+//   }
+//   return arr;
+// }
 
-// console.log(people);
+// console.log(
+//   shortByAge([
+//     { name: "A", age: 25 },
+//     { name: "B", age: 20 },
+//     { name: "C", age: 30 },
+//     { name: "FF", age: 50 },
+//     { name: "DD", age: 10 },
+//   ]),
+// );
 
 // ***********************************************************************************************************
 
 // 6.  Sort an Array but Keep Zeros at the End (Zeros Fixed)
 
-// let arr = [3, 0, 5, 0, 2, 1];
+// function zerosAtLast(arr) {
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 0) {
+//       arr.splice(i, 1);
+//       count++;
+//     }
+//   }
 
-// let nonZero = arr.filter((num) => num !== 0).sort((a, b) => a - b);
-// let zeros = arr.filter((num) => num === 0);
-// let result = [...nonZero, ...zeros];
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
 
-// console.log(result);
+//   for (let i = 0; i < count; i++) {
+//     arr.push(0);
+//   }
+//   return arr;
+// }
+
+// console.log(zerosAtLast([3, 0, 5, 0, 2, 1]));
+
+// function zerosAtLast(arr) {
+//   const nonZeros = arr.filter((el) => el !== 0).sort((a, b) => a - b);
+//   const zeros = arr.filter((el) => el == 0);
+
+//   return [...nonZeros, ...zeros];
+// }
+
+// console.log(zerosAtLast([3, 0, 5, 0, 2, 1]));
 
 // ***********************************************************************************************************
 
@@ -146,39 +268,50 @@
 // 1.  Sort an Array and Track How Many Comparisons Were Made
 
 // function sortAndCountComparisons(arr) {
-//   let comparisons = 0;
-//   let n = arr.length;
-//   let a = [...arr];
+//   let count = 0;
 
-//   for (let i = 0; i < n - 1; i++) {
-//     for (let j = 0; j < n - i - 1; j++) {
-//       comparisons++;
-//       if (a[j] > a[j + 1]) {
-//         let temp = a[j];
-//         a[j] = a[j + 1];
-//         a[j + 1] = temp;
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let swap = false;
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       count++;
+//       if (arr[j] > arr[j + 1]) {
+//         swap = true;
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
 //       }
 //     }
+
+//     if (!swap) break;
 //   }
 
-//   return { sortedArray: a, comparisons };
+//   return { arr, count: `${count} times Comparison` };
 // }
 
-// const result = sortAndCountComparisons([5, 1, 4, 2, 8]);
-// console.log(result.sortedArray);
-// console.log(result.comparisons);
-
+// console.log(sortAndCountComparisons([5, 1, 4, 2, 8]));
 // ***********************************************************************************************************
 
 // 2. Sort an Array of Characters Case-Insensitive
 
 // function sortCharsCaseInsensitive(arr) {
-//   return arr.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let swap = false;
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       if (arr[j].toLowerCase() > arr[j + 1].toLowerCase()) {
+//         swap = true;
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+
+//     if (!swap) break;
+//   }
+
+//   return arr;
 // }
 
-// const result = sortCharsCaseInsensitive(["b", "A", "d", "C"]);
-// console.log(result);
-
+// console.log(sortCharsCaseInsensitive(["b", "A", "d", "C", "Z", "z", "B"]));
 // ***********************************************************************************************************
 
 // 3.  Sort a 2D Array by the First Element of Each Subarray
@@ -235,15 +368,15 @@
 
 // 6.  Sort an Array of Strings by Length (Shortest to Longest)
 
-function sortByLength(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j].length > arr[j + 1].length) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
-    }
-  }
-  return arr;
-}
+// function sortByLength(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - i - 1; j++) {
+//       if (arr[j].length > arr[j + 1].length) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// }
 
-console.log(sortByLength(["hi", "javascript", "is", "fun"]));
+// console.log(sortByLength(["hi", "javascript", "is", "fun"]));

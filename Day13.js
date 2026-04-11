8eab8ed8457160da46d9f06bb4852221d49bf0af
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-04-10 22:26:40 -0400

    Sort an Array of Characters by ASCII Value

--- Day13.js
+++ Day13.js
@@ -118,27 +118,75 @@
 
 // console.log(selectionSortTrackMinIndex([4, 2, 5, 3, 1]));
 
+// function selectionSortTrackMinIndex(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let minIndex = i;
+
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[minIndex]) {
+//         minIndex = j;
+//       }
+//     }
+
+//     let temp = arr[minIndex];
+//     arr[minIndex] = arr[i];
+//     arr[i] = temp;
+//   }
+
+//   return arr;
+// }
+
+// console.log(selectionSortTrackMinIndex([4, 2, 5, 3, 1, -8]));
+
 // ***********************************************************************************************************
 
 // 5. Sort an Array of Objects by Name
 
-// const arr = [{ name: "Charlie" }, { name: "Alice" }, { name: "Bob" }];
+// function sortByName(arr) {
+//   // arr.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+//   // arr.sort(
+//   //   (a, b) =>
+//   //     a.name.toLowerCase().charCodeAt() - b.name.toLowerCase().charCodeAt(),
+//   // );
 
-// arr.sort((a, b) => a.name.localeCompare(b.name));
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let minIndex = i;
 
-// console.log(arr);
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j].name.toLowerCase() < arr[minIndex].name.toLowerCase()) {
+//         minIndex = j;
+//       }
+//     }
+
+//     if (minIndex !== i) {
+//       let temp = arr[i];
+//       arr[i] = arr[minIndex];
+//       arr[minIndex] = temp;
+//     }
+//   }
+
+//   return arr;
+// }
+
+// console.log(
+//   sortByName([
+//     { name: "Charlie" },
+//     { name: "Alice" },
+//     { name: "Bob" },
+//     { name: "bimal" },
+//     { name: "aalic" },
+//   ]),
+// );
 
 // ***********************************************************************************************************
 
 // 6. Find the K Largest Elements Without Full Sorting
 
 // function kLargestElements(arr, k) {
-//   let n = arr.length;
-
 //   for (let i = 0; i < k; i++) {
 //     let maxIndex = i;
 
-//     for (let j = i + 1; j < n; j++) {
+//     for (let j = i + 1; j < arr.length; j++) {
 //       if (arr[j] > arr[maxIndex]) {
 //         maxIndex = j;
 //       }
@@ -161,16 +209,18 @@
 // 1. Sort a 2D Array by Second Element in Each Subarray
 
 // function bubbleSort2D(arr) {
-//   let n = arr.length;
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let minValue = i;
 
-//   for (let i = 0; i < n - 1; i++) {
-//     for (let j = 0; j < n - i - 1; j++) {
-//       if (arr[j][1] > arr[j + 1][1]) {
-//         let temp = arr[j];
-//         arr[j] = arr[j + 1];
-//         arr[j + 1] = temp;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j][1] < arr[minValue][1]) {
+//         minValue = j;
 //       }
 //     }
+
+//     let temp = arr[i];
+//     arr[i] = arr[minValue];
+//     arr[minValue] = temp;
 //   }
 
 //   return arr;
@@ -178,9 +228,9 @@
 
 // console.log(
 //   bubbleSort2D([
-//     [3, 9],
-//     [1, 4],
-//     [2, 5],
+//     [5, 2],
+//     [8, 1],
+//     [3, 7],
 //   ]),
 // );
 
@@ -208,34 +258,58 @@
 
 // 3. Sort Only Elements at Even Indexes
 
-function sortEvenIndex(arr) {
-  let even = [];
+// function sortEvenIndex(arr) {
+//   let even = [];
 
-  for (let i = 0; i < arr.length; i += 2) {
-    even.push(arr[i]);
-  }
+//   for (let i = 0; i < arr.length; i += 2) {
+//     even.push(arr[i]);
+//   }
 
-  for (let i = 0; i < even.length - 1; i++) {
+//   for (let i = 0; i < even.length - 1; i++) {
+//     let minIndex = i;
+
+//     for (let j = i + 1; j < even.length; j++) {
+//       if (even[j] < even[minIndex]) {
+//         minIndex = j;
+//       }
+//     }
+
+//     let temp = even[i];
+//     even[i] = even[minIndex];
+//     even[minIndex] = temp;
+//   }
+
+//   let k = 0;
+
+//   for (let i = 0; i < arr.length; i += 2) {
+//     arr[i] = even[k++];
+//   }
+
+//   return arr;
+// }
+
+// console.log(sortEvenIndex([9, 4, 7, 6, 3, 2]));
+
+// ***********************************************************************************************************
+
+// 4. Sort an Array of Characters by ASCII Value
+
+function sortChars(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
     let minIndex = i;
 
-    for (let j = i + 1; j < even.length; j++) {
-      if (even[j] < even[minIndex]) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j].charCodeAt(0) < arr[minIndex].charCodeAt(0)) {
         minIndex = j;
       }
     }
 
-    let temp = even[i];
-    even[i] = even[minIndex];
-    even[minIndex] = temp;
-  }
-
-  let k = 0;
-
-  for (let i = 0; i < arr.length; i += 2) {
-    arr[i] = even[k++];
+    let temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
   }
 
   return arr;
 }
 
-console.log(sortEvenIndex([9, 4, 7, 6, 3, 2]));
+console.log(sortChars(["d", "A", "c", "B"]));

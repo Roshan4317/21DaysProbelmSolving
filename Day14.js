864c02ce961d81f1b1e95d5b50def9f3467fa023
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-04-12 22:52:46 -0400

    Sort an Array in Descending Order

--- Day14.js
+++ Day14.js
@@ -2,11 +2,32 @@
 
 // 1. Sort an Array in Ascending Order Using Insertion Sort
 
-function insertionSortAsc(arr) {
+// function insertionSortAsc(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     let current = arr[i];
+//     let j = i - 1;
+//     while (j >= 0 && arr[j] > current) {
+//       arr[j + 1] = arr[j];
+//       j--;
+//     }
+
+//     arr[j + 1] = current;
+//   }
+
+//   return arr;
+// }
+
+// console.log(insertionSortAsc([9, 5, 1, 4, 3]));
+
+// ***********************************************************************************************************
+
+// 2. Sort an Array in Descending Order
+
+function insertionSortDesc(arr) {
   for (let i = 1; i < arr.length; i++) {
     let current = arr[i];
     let j = i - 1;
-    while (j >= 0 && arr[j] > current) {
+    while (j >= 0 && arr[j] < current) {
       arr[j + 1] = arr[j];
       j--;
     }
@@ -17,4 +38,4 @@ function insertionSortAsc(arr) {
   return arr;
 }
 
-console.log(insertionSortAsc([9, 5, 1, 4, 3]));
+console.log(insertionSortDesc([9, 5, 1, 4, 3]));
